837f016d635e9813c7abf40ab9a9b959d9ccae00
electronyoon <electronyoon@gmail.com>
2024-12-15 18:35:01 +0900

    chore: add SpringBoot3

---
+++ src/main/java/io/celebe/challenge/CelebeChallengeApplication.java
@@ -0,0 +1,13 @@
+package io.celebe.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelebeChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelebeChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/celebe/challenge/CelebeChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.celebe.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelebeChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
