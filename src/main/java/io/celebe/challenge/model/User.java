0eea1e4c4bd6c6514adfd786ccfe25298556b615
electronyoon <electronyoon@gmail.com>
2024-12-16 18:17:35 +0900

    #3 feat: add user model

---
+++ src/main/java/io/celebe/challenge/model/User.java
@@ -0,0 +1,38 @@
+package io.celebe.challenge.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class User {
+    private Long id;
+    private String publicId;
+    private String email;
+    private String nickname;
+    private String name;
+    private String bio;
+    private String link1;
+    private String link2;
+    private String thumbnailUrl;
+    private Integer followerCount;
+    private Integer followingCount;
+    private Integer postCount;
+
+    @JsonIgnore
+    private Boolean isActive;
+    @JsonIgnore
+    private LocalDateTime deactivatedAt;
+    private LocalDateTime createdAt;
+    @JsonIgnore
+    private LocalDateTime updatedAt;
+
+    private Boolean isFollowing;
+    private Boolean isFollowedBy;
+}
+
