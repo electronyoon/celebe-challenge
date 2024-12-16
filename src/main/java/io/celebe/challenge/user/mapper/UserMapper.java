f66d4ce2412f24bba71d39b3ae297c75f768a969
electronyoon <electronyoon@gmail.com>
2024-12-16 21:30:44 +0900

    #8 feat: add 6-digit unique ID generator

--- src/main/java/io/celebe/challenge/user/mapper/UserMapper.java
+++ src/main/java/io/celebe/challenge/user/mapper/UserMapper.java
@@ -1,10 +1,11 @@
 package io.celebe.challenge.user.mapper;
 
-import io.celebe.challenge.model.dto.User;
+import io.celebe.challenge.model.User;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMapper {
     User selectUserByPublicId(String publicId);
 
+    Boolean existsByPublicId(String publicId);
 }
