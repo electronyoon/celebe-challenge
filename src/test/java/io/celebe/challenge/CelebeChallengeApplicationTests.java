38f26c036dda75fee8e44ad9ac0e83fb7d3eb3df
electronyoon <electronyoon@gmail.com>
2024-12-19 15:31:39 +0900

    #28 test: reorganize test class dependencies

--- src/test/java/io/celebe/challenge/CelebeChallengeApplicationTests.java
+++ src/test/java/io/celebe/challenge/CelebeChallengeApplicationTests.java
@@ -1,13 +1,12 @@
 package io.celebe.challenge;
 
-import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
-class CelebeChallengeApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
+public class CelebeChallengeApplicationTests {}
--- src/test/java/io/celebe/challenge/follow/FollowApiTest.java
+++ src/test/java/io/celebe/challenge/follow/FollowApiTest.java
@@ -1,26 +1,19 @@
 package io.celebe.challenge.follow;
 
+import io.celebe.challenge.CelebeChallengeApplicationTests;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@ActiveProfiles("test")
-@Transactional
 @Sql("/test-sql/follow-api-test.sql")
-class FollowApiTest {
+class FollowApiTest extends CelebeChallengeApplicationTests {
 
     @Autowired
     private MockMvc mockMvc;
