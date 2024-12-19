f0dc132d4062972266ec5dd0c5287cdccaca0a8a
electronyoon <electronyoon@gmail.com>
2024-12-19 18:02:00 +0900

    #4 fix: add missing message

--- src/main/java/io/celebe/challenge/common/constant/ErrorMessage.java
+++ src/main/java/io/celebe/challenge/common/constant/ErrorMessage.java
@@ -2,6 +2,7 @@ package io.celebe.challenge.common.constant;
 
 public class ErrorMessage {
     public static final String USER_NOT_FOUND = "존재하지 않는 유저입니다.";
+    public static final String TARGET_USER_NOT_FOUND = "대상이 존재하지 않거나 비활성화된 계정입니다";
     public static final String USER_INACTIVE = "비활성화된 계정입니다.";
     public static final String FOLLOW_SELF = "자기 자신을 팔로우할 수 없습니다.";
     public static final String ALREADY_FOLLOWING = "이미 팔로우하고 있는 계정입니다.";
